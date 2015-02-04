0435e20f56b9f51b292fc5a4a62948952e4cf672
Jon Davis <jon@snowulf.com>
2015-02-04 10:53:19 -0800

    Cleaned up. Comments. nick change added & username tracking

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # socketio-sample
 Jon's socket.io Sample
+
+# Resources
+* http://socket.io/docs/
+* http://socket.io/get-started/chat/
+* https://github.com/rauchg/chat-example
+* https://github.com/Automattic/socket.io/tree/master/examples/chat
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var version = 2;
+var version = 3;
+var usernames = {};
+
 var io = require('socket.io').listen(3000);
 var chat = io
 	.of('/chat')
 	.on('connection', function (socket) {
 		socket.on('chat', function(msg){
 			var address = socket.handshake.address;
+			var date = new Date();
+			msg.time = date.toISOString();
 			chat.emit('chat', msg);
-			console.log('Chat -- ', address, ' -- ', msg[0], ' -- ', msg[1]);
+			console.log('Chat - '+ address +' - '+ msg.nick +' - '+ msg.time +' - '+ msg.message);
     		});
   	});
 
@@ -24,9 +28,38 @@ var server = io
 	.of('/server')
 	.on('connection', function (socket) {
 		var address = socket.handshake.address;
-		socket.emit('server', "Welcome " + address);
 		socket.emit('version', version);
+
+		socket.on('joinnick', function(msg){
+			usernames[msg] = msg;
+			socket.username = msg;
+			server.emit('server', "Welcome " + msg + " hailing from "+ address);
+			socket.emit('server', "Welcome user " + socket.id);
+			console.log('Nick join: ' + address +' - '+ socket.id +' - '+ msg);
+		});
+
+		socket.on('nickchange', function(msg){
+			var bef = msg[0];
+			var aft = msg[1];
+			console.log('nickchange requested by ' + socket.id +' -- '+ bef +' -- '+ aft);
+			
+			var bannedNames = ["jod","server","god","shakataganai","snofox"];
+
+			if(usernames[aft] != null || socket.username != bef || bannedNames.indexOf(aft.toLowerCase()) > -1 || aft.length < 3){
+				console.log('Rejecting nickchange ' + socket.id);
+				socket.emit('server', "Nick change rejected");
+			}else{
+				socket.emit('nickchange',[bef,aft]);
+				server.emit('server', bef + " is now known as " + aft);
+				delete usernames[bef];
+				usernames[aft] = aft;
+				socket.username = aft;
+				console.log('Completed nickchange ' + socket.id);
+			}
+		});
+
 	});
+
 
 
 io.on('connection', function(socket){
@@ -37,9 +70,10 @@ io.on('connection', function(socket){
 //		console.log("Msg: ",msg);
 //	});
 
-	console.log('Connected: ', address);
+	console.log('Connected: ', address, socket.id);
 
 	socket.on('disconnect', function(){
-		console.log('Disconnected: ', address);
+		delete usernames[socket.username];
+		console.log('Disconnected: ', address, socket.id);
 	});
 });
